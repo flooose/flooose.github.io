4ea90599d7a86333bd19a4a46e0d1ebfd7530e79
christopher floess <chris.floess@jjff.de>
2017-01-01 14:51:15 +0100

    clean up old Rmarkdown stuff

--- _knitr/cleanup.bash
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-rm 2016-07-23-coursera.html  2016-07-23-coursera.knit.md  2016-07-23-coursera.utf8.md  2016-07-23-coursera.md 2016-07-23-coursera_cache/ 2016-07-23-coursera_files/ cache/ figure/ *.pdf -r &> /dev/null
-
-echo ls:
-ls
